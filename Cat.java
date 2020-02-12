2abd4d65366d209839e77ae02d7ac8bd0cb6af59
Airat-asd <asd09820982@yandex.ru>
2020-02-12 16:19:04 +0500

    Lesson 5

---
+++ Cat.java
@@ -0,0 +1,43 @@
+class Lesson5 {
+
+    /*public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.name = "Murzik";
+        cat.color = "white";
+        cat.age = 3;
+        System.out.println("print: "+ cat.name + " " + cat.color + " " + cat.age + " " + cat.voice());
+        //cat.voice();
+    }
+}
+
+class Cat {
+	int age;
+	String color;
+	String name;
+
+	String voice() {
+		System.out.println("meow!");
+        return "meow!";
+	}*/
+    public static void main(String[] args) {
+        Cat cat = new Cat("Murzik", "white", 3);
+        System.out.println("print: "+ cat.name + " " + cat.color + " " + cat.age);
+        cat.voice();
+    }
+}
+
+    class Cat {
+        int age;
+        String color;
+        String name;
+
+        Cat(String name, String color, int age) {
+            this.name = name;
+            this.color = color;
+            this.age = 3;
+        }
+        void voice() {
+            System.out.println("meow!");
+    }
+
+}
