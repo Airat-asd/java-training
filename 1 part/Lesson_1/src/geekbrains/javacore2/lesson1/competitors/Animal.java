40d95482c53917b64c1feba52c77d2ac26151628
Airat-asd <32131437+Airat-asd@users.noreply.github.com>
2020-03-09 17:12:02 +0500

    Delete Animal.java

--- 1 part/Lesson_1/src/geekbrains/javacore2/lesson1/competitors/Animal.java
+++
@@ -1,9 +0,0 @@
-package geekbrains.javacore2.lesson1.competitors;
-
-public class Animal {
-    private int maxRunDistance;
-
-    public Animal(int maxRunDistance) {
-        this.maxRunDistance = maxRunDistance;
-    }
-}
