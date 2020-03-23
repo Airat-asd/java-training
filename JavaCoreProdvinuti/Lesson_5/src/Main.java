40e0a3d4885b7fa0748cf007106a0efa2a81558a
Airat-asd <asd09820982@yandex.ru>
2020-03-23 11:40:12 +0500

    performed

--- JavaCoreProdvinuti/Lesson_5/src/Main.java
+++ JavaCoreProdvinuti/Lesson_5/src/Main.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    static final int SIZE = 10;
-    static final int THREAD = 2;
+    static final int SIZE = 10_000_000;
+    static final int THREAD = 9;
     static final int HALF = SIZE / THREAD;
 
     public static void main(String[] args) {
@@ -21,52 +21,45 @@ public class Main {
         long a = System.currentTimeMillis();
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            System.out.println("Метод1 arr["+i+"]="+arr1[i]);
-
+//            System.out.println("Метод1 arr["+i+"]="+arr1[i]);
         }
         System.out.println("Метод1: Время заполения - " + (System.currentTimeMillis() - a));
-//        System.out.println("Метод1 arr[0] "+arr1[0]);
-//        System.out.println("Метод1 arr["+HALF+"] "+arr1[HALF]);
     }
 
     public static void method2() {
         float[] arr2 = new float[SIZE];
-        float[] arr2inc = new float[HALF];
+        float[][] arr2inc = new float[THREAD][HALF];
         ArrayList<MyRunnable> function = new ArrayList<>();
         Thread[] t = new Thread[THREAD];
         Arrays.fill(arr2, 1);
         long a = System.currentTimeMillis();
-        for (int i = 0; i < THREAD; i++) {
-            for (int j = 0; j < HALF; j++) {
-                arr2inc[j] = arr2[j + HALF * i];
+        for (int j = 0; j < THREAD; j++) {
+            for (int i = 0; i < HALF; i++) {
+                arr2inc[j][i] = arr2[i+j*HALF];
             }
+        }
+        for (int i = 0; i < THREAD; i++) {
             function.add(new MyRunnable(arr2inc, i, HALF));
         }
-//        for (int i = 0; i < THREAD; i++) {
-//            function.add(new MyRunnable(arr2inc, i, HALF));
-//        }
         for (int i = 0; i < THREAD; i++) {
             t[i] = new Thread(function.get(i));
+        }
+        for (int i = 0; i < THREAD; i++) {
             t[i].start();
         }
-//        MyRunnable function1 = new MyRunnable(arr2inc1, 0);
-//        MyRunnable function2 = new MyRunnable(arr2inc2, HALF);
-//        Thread t1 = new Thread(function1);
-//        Thread t2 = new Thread(function2);
-//        t1.start();
-//        t2.start();
         try {
-            t[0].join();
-            t[1].join();
+            for (int i = 0; i < THREAD; i++) {
+                t[i].join();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.arraycopy(function.get(0).getArr2inc(), 0, arr2, 0, HALF);
-//        for (int i = 0; i < THREAD; i++) {
-//            for (int j = 0; j < HALF; j++) {
-//                arr2[j + HALF * i] = arr2inc[i][j];
-//            }
-//        }
+//        System.arraycopy(function.get(0).getArr2inc(), 0, arr2, 0, HALF);
+        for (int i = 0; i < THREAD; i++) {
+            for (int j = 0; j < HALF; j++) {
+                arr2[j + HALF * i] = arr2inc[i][j];
+            }
+        }
         System.out.println("Метод2: Время заполения - " + (System.currentTimeMillis() - a));
     }
 }
--- JavaCoreProdvinuti/Lesson_5/src/MyRunnable.java
+++ JavaCoreProdvinuti/Lesson_5/src/MyRunnable.java
@@ -1,9 +1,9 @@
 public class MyRunnable implements Runnable {
-    private float[] arr2inc;
+    private float[][] arr2pro;
     private int part, position;
 
-    public MyRunnable(float[] arr2inc, int part, int position) {
-        this.arr2inc = arr2inc;
+    public MyRunnable(float[][] arr2pro, int part, int position) {
+        this.arr2pro = arr2pro;
         this.part = part;
         this.position = position;
     }
@@ -11,15 +11,12 @@ public class MyRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < position; i++) {
-            arr2inc[i] = (float) (arr2inc[i] * Math.sin(0.2f + (i + part*position) / 5) * Math.cos(0.2f + (i + part*position) / 5) * Math.cos(0.4f + (i + part*position) / 2));
-            if (i<5) {
-                System.out.println("Часть "+part+" i="+i+" "+(i + part*position)+" - "+ arr2inc[i]);
-//                System.out.println("(arr1["+i+"] * Math.sin(0.2f + "+i+" / 5) * Math.cos(0.2f + "+i+ "/ 5) * Math.cos(0.4f + "+i+ "/ 2));");
-            }
+            arr2pro[part][i] = (float) (arr2pro[part][i] * Math.sin(0.2f + (i + part*position) / 5) * Math.cos(0.2f + (i + part*position) / 5) * Math.cos(0.4f + (i + part*position) / 2));
+//            System.out.println("Часть "+part+" i="+i+" "+(i + part*position)+" = "+ arr2pro[part][i]);
         }
     }
 
-    public float[] getArr2inc() {
-        return arr2inc;
+    public float[][] getArr2inc() {
+        return arr2pro;
     }
 }
